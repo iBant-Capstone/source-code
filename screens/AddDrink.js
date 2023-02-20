1adecdf06f5413ee9d7febcb9f4a31b8eb1cb0fb
Gisele Fox <>
2023-02-20 14:21:37 -0800

    AddDrink unusable but rid of old code

--- screens/AddDrink.js
+++ screens/AddDrink.js
@@ -8,23 +8,22 @@ let styles = StyleSheet.styles;
 
 const AddDrink = ({route, navigation}) => {
 
-    const addDrink = async () => {
-        const data = {
-          name: 'newDrink'
-        };
-        try {
-          await AsyncStorage.setItem(Math.random() * 100 + 'myKey', JSON.stringify(data));
-          console.log('Data saved successfully!');
-        } catch (error) {
-          console.log(error);
-        }
-    }
+    // const addDrink = async () => {
+    //     const data = {
+    //       name: 'newDrink'
+    //     };
+    //     try {
+    //       await AsyncStorage.setItem(Math.random() * 100 + 'myKey', JSON.stringify(data));
+    //       console.log('Data saved successfully!');
+    //     } catch (error) {
+    //       console.log(error);
+    //     }
+    // }
 
     return (
         <View style={styles.centered}>
             <Text>{route.params.title}</Text>
             <Button
-                onPress={addDrink}
                 title="Add Drink"
                 color="#841584"
                 accessibilityLabel="Learn more about this purple button"
@@ -35,11 +34,6 @@ const AddDrink = ({route, navigation}) => {
                 color="#841584"
                 accessibilityLabel="Learn more about this purple button"
             />
-            {
-                route.params.drinks.map((drink, index) => (
-                    <Text key={index}>{drink}</Text>
-                ))
-            }
         </View>
     );
 }
--- screens/BACCalc.js
+++ screens/BACCalc.js
@@ -41,8 +41,9 @@ const BACCalc = ({route, navigation}) => {
         );
     }
 
-    // TODO: figure out how to retrieve and simply display the data drom async storage
+    // TODO: add the right data to the async storage set method
     // TODO: build the cards of the drinks added below the add drink button
+    // TODO: MAYBE move the async keys component to it's own file?
     // TODO: make the BAC number updateable
     // TODO: add a buffer div to the top of the page
 
