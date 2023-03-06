4b5c2233db64ad797297faac5f1ae7692b388994
Melina Perraut <melina@perraut.us>
2023-03-05 19:51:19 -0800

    Made minor edits to BiologicalSexInput

--- screens/onboardingScreens/BiologicalSexInput.js
+++ screens/onboardingScreens/BiologicalSexInput.js
@@ -20,7 +20,7 @@ const BiologicalSex = ({ navigation }) => {
                 Input your biological sex here
                 
                 <Text style={styles.redBoldText}>Please note:</Text>
-                We are using an algorithm that uses  male-bodied and female-bodied individuals as a shortcut for defining body mass, fat distribution, and enzymes. Current research on BAC calculation for trans or intersex individuals is greatly lacking.
+                We are using a BAC algorithm that uses male-bodied and female-bodied individuals as a shortcut for defining body mass, fat distribution, and enzymes. Current research on BAC calculation for trans or intersex individuals is greatly lacking.
             </Text>
             <Footer rightButtonLabel="Next" rightButtonPress={() => { navigation.navigate('InformationHub'); }} leftButtonLabel="Skip" leftButtonPress={() => { navigation.navigate('InformationHub'); }} />
         </View>
