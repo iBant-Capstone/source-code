7fb2097a12b97124e8e1b3800ad077c2743f9bf0
Gisele Fox <>
2023-04-12 11:53:44 -0700

    changed a werid error

--- components/addDrink-components/handleInput.js
+++ components/addDrink-components/handleInput.js
@@ -1,21 +1,19 @@
 import AsyncStorage from "@react-native-async-storage/async-storage"
-import { useState, useEffect} from "react"
 
 const handleInput = async (drinks, newDrink, newKey, newValue, nextPage, navigation ) => {
   try {
-      newDrink[newKey] = newValue
-      if (nextPage == "BACCalc") {
-        drinks.push(newDrink)
-        let drinksToSend = JSON.stringify(drinks)
-        await AsyncStorage.setItem('drinks', drinksToSend)
-        navigation.navigate(nextPage, { drinks: drinks, newDrink: newDrink})
-      } else {
-        navigation.navigate(nextPage, { drinks: drinks, newDrink: newDrink})
-      }
+    newDrink[newKey] = newValue
+    if (nextPage == "BACCalc") {
+      drinks.push(newDrink)
+      let drinksToSend = JSON.stringify(drinks)
+      await AsyncStorage.setItem('drinks', drinksToSend)
+      navigation.navigate(nextPage, { drinks: drinks, newDrink: newDrink})
+    } else {
+      navigation.navigate(nextPage, { drinks: drinks, newDrink: newDrink})
+    }
   } catch (err) {
     console.log(err)
   }
-  
 }
 
 export default handleInput
