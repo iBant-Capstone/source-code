9c8778b4a203c35e58c64ebe3e49bd26d2def5cb
Gisele Fox <>
2023-04-24 13:36:20 -0700

    clean up commented out code

--- components/BACCalc-components/GetHomeSafelySection.js
+++ components/BACCalc-components/GetHomeSafelySection.js
@@ -22,9 +22,7 @@ const GetHomeSafelySection = (props) => {
     return (
         <View style={{ backgroundColor: '#FFFFFF', padding: 15, maxWidth: '90%', borderRadius: 15 }}>
             <Text style={textStyles.redSemiBoldText}>Get Home Safely</Text>
-            {/* <Text style={textStyles.redSemiBoldText}>Your BAC is: {BAC}</Text> */}
             <Text style={textStyles.text}>{warningText}</Text>
-            {/* <br></br> */}
             <Text style={textStyles.text}>For your own safety and for the safety of everyone else on the road, please don't drink and drive regardless of whether your BAC is below the federal limit. We recommend getting an Uber, riding with a designated driver, calling someone you trust, walking, or using public transit instead.</Text>
             <GetHomeSafelyButtons />
         </View>
